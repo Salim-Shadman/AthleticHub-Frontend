4d0aee13dc6ff8a38ef656b66cf6f712da90bb25
shadman <shadmansalimameo12@gmail.com>
2025-08-10 15:57:06 +0600

    updated rootlayout.jsx file

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,27 +1,42 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../pages/shared/Navbar';
 import Footer from '../pages/shared/Footer';
-
+import { useEffect } from 'react'; 
 
 const RootLayout = () => {
 
+    useEffect(() => {
+      
+        const handleThemeChange = (e) => {
+            if (e.matches) {
+                document.documentElement.setAttribute('data-theme', 'dark');
+            } else {
+                document.documentElement.setAttribute('data-theme', 'light');
+            }
+        };
+
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        
+        
+        handleThemeChange(mediaQuery);
+
+        
+        mediaQuery.addEventListener('change', handleThemeChange);
+
+        
+        return () => {
+            mediaQuery.removeEventListener('change', handleThemeChange);
+        };
+    }, []);
+
     return (
         <div className="flex flex-col min-h-screen">
-
             <Navbar />
-
             <main className="flex-grow">
-                
                 <Outlet />
-
             </main>
-
             <Footer />
         </div>
-
-
-
-
     );
 };
 
