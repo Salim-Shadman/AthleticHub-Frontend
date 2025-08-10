daba0cc6cadbed015045200f1151840fe0659c4d
shadman <shadmansalimameo12@gmail.com>
2025-08-10 16:01:30 +0600

    main.jsx file updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import './index.css';
-
 import router from './router/router.jsx';
 import { RouterProvider } from 'react-router';
 import AuthProvider from './contexts/AuthContext/AuthProvider.jsx';
-
+import ThemeProvider from './contexts/ThemeContext.jsx'; 
 
 const rootElement = document.getElementById('root');
-
-
 const root = createRoot(rootElement);
 
-
 root.render(
-
   <StrictMode>
-
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-    
-  </StrictMode>,
-
-
+   
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
 );
